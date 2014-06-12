10c783216346c55e39c1994e268fdedb529b6d62
Evan O'Keeffe <evanokeeffe@gmail.com>
2014-06-12 14:55:15 +0100

    "added a spotify installer"

---
+++ install_spotify.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+sudo apt-get update
+sudo apt-get install spotify-client
